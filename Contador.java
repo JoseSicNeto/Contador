669996b9e191612280944fd897f724f2702022a5
José S Neto <136672284+JoseSicNeto@users.noreply.github.com>
2023-07-13 17:42:57 -0300

    Add files via upload

---
+++ Contador.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro parâmetro.");
+    }
+}
+
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois <= parametroUm) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + (parametroUm + i));
+        }
+    }
+}
